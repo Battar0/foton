09aa9e6eda8f8f58c64080d26a0767ce97bf89bf
b2198 <onerb98@hotmail.com>
2018-11-14 15:43:36 -0200

    Criação das classes Formulario e Pergunta
    De acordo com o UML inicial

---
+++ src/regras_de_negocio/Formulario.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regras_de_negocio;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author b2198
+ */
+public class Formulario {
+    public String nome;
+    public String descricao;
+    public LocalTime dataInicio;
+    public LocalTime dataTermino;
+    public String nomeAutor;
+    public ArrayList<Pergunta> questao;
+
+    public Formulario(){
+        
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public LocalTime getDataInicio() {
+        return dataInicio;
+    }
+
+    public LocalTime getDataTermino() {
+        return dataTermino;
+    }
+
+    public boolean setNome(String nome) {
+        this.nome = nome;
+        
+        return true;
+    }
+
+    public boolean setDescricao(String descricao) {
+        this.descricao = descricao;
+        
+        return true;
+    }
+
+    public boolean setDataInicio(LocalTime dataInicio) {
+        this.dataInicio = dataInicio;
+        
+        return true;
+    }
+
+    public boolean setDataTermino(LocalTime dataTermino) {
+        this.dataTermino = dataTermino;
+        
+        return true;
+    }
+    
+    
+}
---
+++ src/regras_de_negocio/Pergunta.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regras_de_negocio;
+
+/**
+ *
+ * @author b2198
+ */
+public class Pergunta {
+    public String texto;
+    public byte tipo;
+
+    public String getTexto() {
+        return texto;
+    }
+    
+    public boolean setTexto(String texto){
+        this.texto = texto;
+        
+        return true;
+    }
+
+    public void setTipo(byte tipo) {
+        this.tipo = tipo;
+    }
+    
+    private boolean checaTexto(){
+        
+        
+        return true;
+    }
+}
