841dd14e4c9f8f0ee0ae5c74d7d8dc7feead0a65
b2198 <onerb98@hotmail.com>
2018-11-21 15:27:53 -0200

    Criação da classe Main e integração com IntelliJ Idea
    A classe Main é a que será chamada na inicialização do programa e será responsável pela inicialização da UI. A integração com o IntelliJ Idea não intefere com o NetBeans.

---
+++ src/UI/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author b2198
+ */
+public class Main extends Application {
+    
+    public static void main(String[] args){
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+        primaryStage.show();
+    }
+    
+}
