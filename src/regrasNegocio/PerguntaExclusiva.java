348940f650a35c8dbbc3908c6f79317edab0fa36
b2198 <onerb98@hotmail.com>
2018-12-05 16:27:48 -0200

    alterações no código e javadoc de regrasNegocio e criação de subclasses de PerguntaFechada
    Javadoc para regrasNegocio completamente implementado, classes PerguntaAlternativa, PerguntaExclusiva e PerguntaLista adicionadas. Alguns métodos e atributos renomeados para fins de clarificação.

---
+++ src/regrasNegocio/PerguntaExclusiva.java
@@ -0,0 +1,20 @@
+package regrasNegocio;
+
+/**
+ * Classe para perguntas exclusivas
+ *
+ * @author b2198
+ */
+public class PerguntaExclusiva extends PerguntaOpcaoUnica {
+
+    /**
+     * Cria uma pergunta exclusiva e inicializa os atributos texto e alternativas
+     *
+     * @param texto o texto da pergunta
+     * @param alternativas as alternativas da pergunta
+     */
+    public PerguntaExclusiva(String texto, String[] alternativas) {
+        super(texto, alternativas);
+    }
+
+}
--- src/regrasNegocio/PerguntaFechada.java
+++ src/regrasNegocio/PerguntaFechada.java
@@ -1,22 +1,41 @@
 package regrasNegocio;
 
+/**
+ * Classe para todas as perguntas que envolvem alternativas
+ *
+ * @author b2198
+ */
 public class PerguntaFechada extends Pergunta {
 
+    /**
+     * as alternativas da pergunta fechada
+     */
     private String[] alternativas;
 
+    /**
+     * Cria uma pergunta fechada e inicializa os atributos texto e alternativas
+     *
+     * @param texto o texto da pergunta
+     * @param alternativas as alternativas da pergunta
+     */
     public PerguntaFechada(String texto, String[] alternativas){
         super(texto);
         setAlternativas(alternativas);
     }
 
+    /**
+     * Seta as respostas da pergunta, verificando se não há respostas repetidas ou que não estão nas alternativas
+     *
+     * @param respostas as respostas da pergunta
+     */
     @Override
-    public boolean setResposta(String[] resposta) {
+    public void setRespostas(String[] respostas) {
 
         //checa se não há respostas repetidas
-        for(int i = 0; i < resposta.length-1; i++){
-            for(int j = i; j < resposta.length; j++){
-                if(resposta[i].equals(resposta[j])){
-                    return false;
+        for(int i = 0; i < respostas.length-1; i++){
+            for(int j = i; j < respostas.length; j++){
+                if(respostas[i].equals(respostas[j])){
+                    //lança exceção
                 }
             }
         }
@@ -25,8 +44,8 @@ public class PerguntaFechada extends Pergunta {
         boolean valida = false;
         for(int i = 0; i < alternativas.length; i++){
             valida = false;
-            for(int j = 0; j < resposta.length; j++) {
-                if (alternativas[i].equals(resposta[j])) {
+            for(int j = 0; j < respostas.length; j++) {
+                if (alternativas[i].equals(respostas[j])) {
                     valida = true;
                     break;
                 }
@@ -38,33 +57,57 @@ public class PerguntaFechada extends Pergunta {
 
         //se a resposta é valida seta o atributo resposta em Pergunta
         if(valida) {
-            setRespostaInterno(resposta);
-            return true;
+            setRespostasInterno(respostas);
+        } else{
+            //lança exceção
         }
-
-        return false;
     }
 
-    public boolean setResposta(int... numerosRespostas){
+    /**
+     * Seta as respostas da pergunta a partir dos índices das alternativas, verificando se não há respostas repetidas
+     *
+     * @param numerosRespostas os índices das respostas da pergunta
+     */
+    public void setResposta(int... numerosRespostas){
+
+        for(int i = 0; i < numerosRespostas.length-1; i++){
+            for(int j = i; j < numerosRespostas.length; j++){
+                if(numerosRespostas[i] == numerosRespostas[j]){
+                    //lança exceção
+                }
+            }
+        }
 
         String[] resposta = new String[numerosRespostas.length];
         for(int i = 0; i < numerosRespostas.length; i++){
             resposta[i] = alternativas[numerosRespostas[i]];
         }
 
-        setRespostaInterno(resposta);
-
-        return true;
+        setRespostasInterno(resposta);
     }
 
+    /**
+     *
+     * @return o número de alternativas desta pergunta fechada
+     */
     public int getNumeroAlternativas(){
         return alternativas.length;
     }
 
+    /**
+     *
+     * @param alternativa o índice da alternativa
+     * @return a alternativa no índice indicado
+     */
     public String getAlternativa(int alternativa){
         return alternativas[alternativa];
     }
 
+    /**
+     * Seta as alternativas da pergunta fechada
+     *
+     * @param alternativas as alternativas da pergunta fechada
+     */
     public void setAlternativas(String[] alternativas) {
         this.alternativas = alternativas;
     }
